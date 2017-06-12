76326c507b6620fffc4b0c2acc0814243dcf9df4
Gaurav Yeole <Gaurav Yeole>
2017-06-12 11:15:26 -0400

    main.js remove coments

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 'use strict';
 
-//var app = require('app');
 const {app, BrowserWindow} = require('electron');
-//var BrowserWindow = require('browser-window');
+
 
 var mainWindow = null;
 
@@ -13,7 +12,6 @@ app.on('ready', function() {
         icon:'idigbio_media_appliance/static/img/icon.png'
     });
 
-    //mainWindow.loadURL("http://www.google.com");
     mainWindow.maximize();
     mainWindow.loadURL('file://' + __dirname+ '/idigbio_media_appliance/templates/index.html');
 });
